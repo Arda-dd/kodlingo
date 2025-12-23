fee5d079fe62d82669ab00d3163fe0fa9d451a31
Arda-dd <ardatnmzoglu@gmail.com>
2025-12-24 01:17:55 +0300

    commit10

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,44 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // <-- YENİ EKLENDİ
 import 'package:flutter_riverpod/flutter_riverpod.dart'
     hide ChangeNotifierProvider, Provider;
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart'; // <-- YENİ EKLENDİ (Az önce oluşan dosya)
 import 'core/routes/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'data/providers/user_local_provider.dart';
 import 'domain/notifiers/user_notifier.dart';
 
-// Ana uygulama başlatma noktası
-void main() {
+void main() async {
+  // <-- async EKLENDİ
+  WidgetsFlutterBinding
+      .ensureInitialized(); // <-- YENİ EKLENDİ (Hata almamak için şart)
+
+  // Firebase'i başlatıyoruz
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
-    // ProviderScope ve ChangeNotifierProvider ile sarılır
     ProviderScope(
       child: ChangeNotifierProvider(
         create: (context) => UserNotifier(UserLocalProvider())..loadUser(),
-        child: const LoopageApp(), // <-- GÜNCELLENDİ: MyApp -> LoopageApp
+        child: const LoopageApp(),
       ),
     ),
   );
 }
 
 class LoopageApp extends StatelessWidget {
-  // <-- GÜNCELLENDİ: Sınıf Adı
   const LoopageApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Temayı CORE katmanından karanlık temayı alacak şekilde güncelledik.
     return MaterialApp(
-      title: 'Loopage', // <-- GÜNCELLENDİ: Görünen İsim
+      title: 'Loopage',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme,
       initialRoute: AppRouter.home,
